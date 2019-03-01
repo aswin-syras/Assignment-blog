c6717f16a35c692c554a7b1d8401f366be0459e4
aswin-syras <47082111+aswin-syras@users.noreply.github.com>
2019-03-01 18:59:29 +0530

    Updated alignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
-def new
-end
-def show
+  def new
+  end
+  def show
     @post = Post.find(params[:id])
   end
-def create
+  def create
   @post = Post.new(post_params)
   @post.save
   redirect_to @post
-end
+  end
   private
   def post_params
     params.require(:post).permit(:title, :text)
   end
-end
+ end
