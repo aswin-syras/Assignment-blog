06d5f7e3a5340e0f1ddf25004443f4248bd5ab4d
Aswin Syars <you@example.com>
2019-03-01 20:16:18 +0530

    Show function added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,20 @@
 class PostsController < ApplicationController
-def new
-end
-def show
+  def new
+  end
+  def show
     @post = Post.find(params[:id])
   end
-def create
-  #render plain: params[:post].inspect
-  @post = Post.new(post_params)
-  @post.save
-  redirect_to @post
-end
-  private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def index
+    @posts = Post.all
   end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+ 
 end
