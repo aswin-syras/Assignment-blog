3764263f2791bdb84ef3911db06fe105fc6a828a
aswin-syras <47082111+aswin-syras@users.noreply.github.com>
2019-03-01 19:00:02 +0530

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   def create
-  @post = Post.new(post_params)
-  @post.save
-  redirect_to @post
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
   private
   def post_params
     params.require(:post).permit(:title, :text)
   end
- end
+end
